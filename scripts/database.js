ef141de20e621552358e65d5ecdf3ac9419af743
Caleb Thompson <calebjthompson1230@gmail.com>
2021-12-01 09:59:33 -0600

    Added database containing information regarding Martin's fish

---
+++ scripts/database.js
@@ -0,0 +1,32 @@
+const database = {
+    fish: [
+        {
+            name: "Terminator",
+            species: "Betta",
+            length: 3,
+            location: "Deep Ocean",
+            diet: "Other Fish"
+        },
+        {
+            name: "Lil' Octo",
+            species: "Umbrella Octopus",
+            length: 7,
+            location: "Who Knows Where",
+            diet: "Cute Food"
+        },
+        {
+            name: "Puffy Boy",
+            species: "Pufferfish",
+            length: 2,
+            location: "Ocean Waters",
+            diet: "Air"
+        },
+        {
+            name: "Marly",
+            species: "Zebra Pleco",
+            length: 3,
+            location: "Brazil",
+            diet: "Frozen or Live Meat"
+        }
+    ]
+};
